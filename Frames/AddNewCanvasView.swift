bcc48ca585ac582946aea9208b1ed45ee34bbfa8
Zoe Olson <zoglow2311@gmail.com>
2023-08-02 11:42:43 -0400

    Finish app tutorial with basic elements of PencilKit

---
+++ Frames/AddNewCanvasView.swift
@@ -0,0 +1,56 @@
+//
+//  AddNewCanvasView.swift
+//  Frames
+//
+//  Created by Zoe Olson on 7/25/23.
+//
+
+import SwiftUI
+
+struct AddNewCanvasView: View {
+    
+    @Environment (\.managedObjectContext) var viewContext
+    @Environment (\.presentationMode) var presentationMode
+    
+    @State private var canvasTitle = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Canvas Title", text: $canvasTitle)
+                }
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .navigationTitle(Text("Add New Canvas"))
+            .navigationBarItems(leading: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image(systemName: "xmark")
+            }), trailing: Button(action: {
+                if !canvasTitle.isEmpty {
+                    let frame = Frame(context: viewContext)
+                    frame.title = canvasTitle
+                    frame.id = UUID()
+                    
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print(error)
+                    }
+                    
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+                
+            }, label: {
+                Text("Save")
+            }))
+        }
+    }
+}
+
+struct AddNewCanvasView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewCanvasView()
+    }
+}
---
+++ Frames/DrawingView.swift
@@ -0,0 +1,26 @@
+//
+//  DrawingView.swift
+//  Frames
+//
+//  Created by Zoe Olson on 8/1/23.
+//
+
+import SwiftUI
+
+struct DrawingView: View {
+   
+    @Environment(\.managedObjectContext) var viewContext
+    
+    @State var id:UUID?
+    @State var data:Data?
+    @State var title:String?
+    
+    var body: some View {
+        VStack {
+            DrawingCanvasView(data: data ?? Data(), id: id ?? UUID())
+                .environment(\.managedObjectContext, viewContext)
+                .navigationTitle(title ?? "Untitled")
+        }
+    }
+    
+}
